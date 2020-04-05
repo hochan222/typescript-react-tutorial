5d1796e6a8c4aee4459430b6a38c2b614101c525
hochan222 <201502766@hufs.ac.kr>
2020-04-05 21:16:14 +0900

    [Todo] add addTodo, section

--- src/Components/Pages/Todo/Todo.tsx
+++ src/Components/Pages/Todo/Todo.tsx
@@ -2,13 +2,34 @@ import React, { useState } from "react";
 
 interface Props {}
 
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+
 export const Todo: React.FC<Props> = () => {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    // preventDefault will stop the form from doing a refresh when submitted
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
       <h1>Todo List</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={value}
@@ -17,6 +38,12 @@ export const Todo: React.FC<Props> = () => {
         />
         <button type="submit">Add Todo</button>
       </form>
+
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <div key={index}>{todo.text}</div>
+        ))}
+      </section>
     </React.Fragment>
   );
 };
